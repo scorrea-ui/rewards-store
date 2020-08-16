23d738a0309569f2fb87ead2feef504100c0154c
Santiago Correa <santiagocorrea@Santiagos-MacBook-Pro.local>
2020-08-16 00:08:05 -0500

    Updates

---
+++ precache-manifest.35f7f493093a7f8ee66f9d79d3595692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b954cdd4c2df577d04291a6da90dda9e",
+    "url": "/rewards-store/index.html"
+  },
+  {
+    "revision": "18e4bffa209586e065cf",
+    "url": "/rewards-store/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "c1ff9743c14b18c63240",
+    "url": "/rewards-store/static/css/main.389efb9d.chunk.css"
+  },
+  {
+    "revision": "18e4bffa209586e065cf",
+    "url": "/rewards-store/static/js/2.86240d8e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rewards-store/static/js/2.86240d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ff9743c14b18c63240",
+    "url": "/rewards-store/static/js/main.1780db5f.chunk.js"
+  },
+  {
+    "revision": "d0e7f59b80373babadee",
+    "url": "/rewards-store/static/js/runtime-main.7586a40e.js"
+  }
+]);
